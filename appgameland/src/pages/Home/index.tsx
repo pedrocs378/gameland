a3e0f25a190d255f52c2aef1e682ffd270fdaa77
Pedro César <pedrocs378@gmail.com>
2021-02-04 18:57:57 -0300

    implemento da tela home

--- appgameland/src/pages/Home/index.tsx
+++ appgameland/src/pages/Home/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { RectButton } from 'react-native-gesture-handler'
 import Icon from 'react-native-vector-icons/Feather'
 
 import { 
 	Container,
 	SearchBox,
 	SearchBoxText,
+	GameSection,
+	HeaderSection,
+	Title,
+	ShowMoreButtonText,
+	Content,
+	Game,
+	GameImage,
 } from './styles'
 
 const Home: React.FC = () => {
@@ -15,6 +23,46 @@ const Home: React.FC = () => {
 				<Icon name="search" size={18} color="#777" />
 				<SearchBoxText>Search...</SearchBoxText>
 			</SearchBox>
+			<GameSection>
+				<HeaderSection>
+					<Title>Popular games</Title>
+					<RectButton>
+						<ShowMoreButtonText>See all</ShowMoreButtonText>
+					</RectButton>
+				</HeaderSection>
+				<Content 
+					pagingEnabled 
+					horizontal 
+					showsHorizontalScrollIndicator={false}
+				>
+					<Game activeOpacity={0.6}>
+						<GameImage
+							resizeMode="cover"
+							source={{ uri: 'https://images.igdb.com/igdb/image/upload/t_thumb/co2s0o.jpg' }}
+						/>
+					</Game>
+				</Content>
+			</GameSection>
+			<GameSection>
+				<HeaderSection>
+					<Title>Releases</Title>
+					<RectButton>
+						<ShowMoreButtonText>See all</ShowMoreButtonText>
+					</RectButton>
+				</HeaderSection>
+				<Content 
+					pagingEnabled 
+					horizontal 
+					showsHorizontalScrollIndicator={false}
+				>
+					<Game activeOpacity={0.6}>
+						<GameImage
+							resizeMode="cover"
+							source={{ uri: 'https://images.igdb.com/igdb/image/upload/t_thumb/co2s0o.jpg' }}
+						/>
+					</Game>
+				</Content>
+			</GameSection>
 		</Container>
 	)
 }
--- appgameland/src/pages/Home/styles.ts
+++ appgameland/src/pages/Home/styles.ts
@@ -1,15 +1,14 @@
 import styled from 'styled-components/native'
 import { RectButton } from 'react-native-gesture-handler'
 
-export const Container = styled.View`
+export const Container = styled.ScrollView`
 	flex: 1;
-	padding: 0 20px;
 `
 
 export const SearchBox = styled(RectButton)`
 	flex-direction: row;
 	align-items: center;
-	margin-top: 60px;
+	margin: 60px 20px 0;
 	background-color: #e1e1e1;
 	padding: 10px;
 	border-radius: 5px;
@@ -20,3 +19,44 @@ export const SearchBoxText = styled.Text`
 	font-family: 'Poppins-Regular';
 	color: #777;
 `
+
+export const GameSection = styled.View`
+	margin-top: 15px;
+`
+
+export const HeaderSection = styled.View`
+	flex-direction: row;
+	justify-content: space-between;
+	align-items: center;
+	margin: 0 20px 10px;
+`
+
+export const Title = styled.Text`
+	font-family: 'Poppins-SemiBold';
+	font-size: 18px;
+`
+
+export const ShowMoreButtonText = styled.Text`
+	font-family: 'Poppins-Regular';
+	color: #3c90ef;
+`
+
+export const Content = styled.ScrollView`
+	height: 240px;
+	padding-left: 15px;
+`
+
+export const Game = styled.TouchableOpacity`
+	width: 160px;
+	position: relative;
+
+	margin-left: 5px;
+`
+
+export const GameImage = styled.Image`
+	height: 100%;
+	border-radius: 8px;
+	
+`
+
+
